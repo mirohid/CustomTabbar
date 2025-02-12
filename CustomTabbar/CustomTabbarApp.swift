a0aa18ee49764ff833e5312512444395241aec0f
mirohid <mirohid7860@gmail.com>
2025-02-12 16:20:55 +0530

    Initial Commit

---
+++ CustomTabbar/CustomTabbarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabbarApp.swift
+//  CustomTabbar
+//
+//  Created by MacMini6 on 12/02/25.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabbarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainTabbarView()
+        }
+    }
+}
